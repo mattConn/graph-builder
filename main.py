43ceedcdb2c5c20e257c11f292934dcb72ee5dd6
Matthew Connelly <matthew.b.connelly@gmail.com>
2021-01-01 19:25:45 -0500

    added node removal feature

--- main.py
+++ main.py
@@ -14,10 +14,17 @@ def updateAndDraw():
 def index():
     return render_template('index.html',filepath = f'static/img/graph{graph.filehash}.png', data = json.dumps(nx.node_link_data(graph)) ) 
 
+# add and remove node
 @app.route("/addnode")
 def addnode():
     graph.add_node(request.args.get('label'))
     updateAndDraw()
+    return redirect(url_for('index'))
+@app.route("/removenode")
+def removenode():
+    if graph.has_node(request.args.get('label')):
+        graph.remove_node(request.args.get('label'))
+        updateAndDraw()
     return redirect(url_for('index'))
 
 @app.route("/addedge")
