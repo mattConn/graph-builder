b67a0bbfbe7eadb0d629e3e793f0194adca45ecf
Matthew Connelly <matthew.b.connelly@gmail.com>
2021-01-14 20:16:16 -0500

    storing filehash in session

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ import json
 import random
 import os
 import subprocess
-from graphgen import graph, nx
-from flask import Flask, request, render_template, redirect, url_for
+from graphgen import graph, draw, filepath, nx
+from flask import Flask, request, render_template, redirect, url_for, session
 app = Flask(__name__)
 
+app.secret_key = os.urandom(24)
+
 def updateAndDraw():
-    graph.filehash = random.randint(1000,4000)
-    graph.draw(f'static/img/graph{graph.filehash}.png')
+    # os.remove()
+    session['filehash'] = random.randint(1000,4000)
+    draw(filepath % session['filehash'])
 
 @app.route("/")
 def index():
-    return render_template('index.html',filepath = f'static/img/graph{graph.filehash}.png', data = json.dumps(nx.node_link_data(graph)) ) 
+    updateAndDraw()
+    return render_template('index.html',filepath = f'static/img/graph{session["filehash"]}.png', data = json.dumps(nx.node_link_data(graph)) ) 
 
 # add and remove node
 @app.route("/addnode")
 def addnode():
     graph.add_node(request.args.get('label'))
     updateAndDraw()
-    return redirect(url_for('index'))
+    return index()
 @app.route("/removenode")
 def removenode():
     if graph.has_node(request.args.get('label')):
